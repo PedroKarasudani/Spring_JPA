600f93c816c6b6af826849f08760ab1e75afbd0b
Pedro K. L <pedro.karasudani@hotmail.com>
2023-10-05 17:40:49 -0300

    extends CrudRepository

---
+++ spring-data/src/main/java/br/com/alura/spring_data/repository/CargoRepository.java
@@ -0,0 +1,9 @@
+package br.com.alura.spring_data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends CrudRepository {
+
+}
