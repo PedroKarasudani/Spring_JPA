594b4ba857b37123d4c963644e01fffaf230e383
Pedro K. L <pedro.karasudani@hotmail.com>
2023-10-20 22:28:34 -0300

    Exemplo usando JPQL inves de Derived Query

--- spring-data/src/main/java/br/com/alura/spring_data/repository/FuncionarioRepository.java
+++ spring-data/src/main/java/br/com/alura/spring_data/repository/FuncionarioRepository.java
@@ -1,7 +1,9 @@
 package br.com.alura.spring_data.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,11 @@ import br.com.alura.spring_data.orm.Funcionario;
 @Repository
 public interface FuncionarioRepository extends CrudRepository <Funcionario, Integer>{
 	List<Funcionario> findByNome(String nome);
+	
+	//Derived Query funciona, mas nome ficou dificil de leitura
+	List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, Double salario, LocalDate data);
+	
+	//ultilizando JPQL fica assim
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nomeZ AND f.salario >= :salarioZ AND f.dataContratacao = :dataZ")
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nomeZ, Double salarioZ, LocalDate dataZ);
 }
